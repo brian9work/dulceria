f77ca03df126aba844adb2e1376ff08e78188af6
Brian <brian9work@gmail.com>
2025-11-23 17:57:41 -0600

    feat: Implement core e-commerce functionality including product display, shopping cart, and shipping form with new assets and mock data.

---
+++ assets/index.tsx
@@ -0,0 +1,25 @@
+import caramelos from "./caramelos.webp"
+import chicles_menta from "./chicles_menta.webp"
+import chocolate_blanco from "./chocolate_blanco.webp"
+import chocolate_leche from "./chocolate_leche.webp"
+import chocolate_negro from "./chocolate_negro.webp"
+import gomitas_asidas from "./gomitas_asidas.webp"
+import gomitas_osos from "./gomitas_osos.webp"
+import malvavisco from "./malvavisco.webp"
+import paleta from "./paleta.webp"
+
+const images = {
+    store: {
+        caramelos,
+        chicles_menta,
+        chocolate_blanco,
+        chocolate_leche,
+        chocolate_negro,
+        gomitas_asidas,
+        gomitas_osos,
+        malvavisco,
+        paleta
+    }
+}
+
+export default images
